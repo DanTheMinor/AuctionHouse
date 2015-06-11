6a9d9352ed097e830ca2a506ed256bb1d172a272
Guest User <student@epicodus.com>
2015-06-11 12:30:32 -0700

    now only updates current bid if the amount bid is larger

--- controllers/new-bid.js
+++ controllers/new-bid.js
@@ -3,7 +3,8 @@ Auction.NewBidController = Ember.Controller.extend({
   actions: {
     save: function() {
       var bidder = this.store.createRecord('bidder', {
-          name: this.get('name')
+          name: this.get('name'),
+          bid_amount: this.get('bid')
       });
       bidder.save();
 
@@ -11,6 +12,18 @@ Auction.NewBidController = Ember.Controller.extend({
       item.get('bidders').pushObject(bidder);
       item.save();
 
+
+      bidder_bid = this.get('bid');
+      //debugger
+      current_bid = item.get('current_bid')
+      current_bid_int = parseInt(current_bid)
+      bidder_bid_int = parseInt(bidder_bid)
+      if (bidder_bid_int <= current_bid_int) {
+        alert("Your bid must be higher than current bid.")
+      }
+      else {
+        item.set('current_bid', bidder_bid);
+      }
       this.transitionToRoute('item', item.id);
     }
   }
--- router.js
+++ router.js
@@ -4,7 +4,11 @@ Auction.Router.map(function() {
   this.resource('catalog', {path: 'catalogs/:catalog_id'}, function() {
     this.resource('add-item');
   });
-  this.resource('item', {path: 'item/:item_id'});
-  this.resource('new-bid');
-  this.resource('edit-bid');
+  this.resource('item', {path: 'item/:item_id'}, function() {
+    this.resource('new-bid');
+    this.resource('edit-bid');
+  });
+  // this.resource('item', {path: 'item/:item_id'});
+  // this.resource('new-bid');
+  // this.resource('edit-bid');
 });
